f2979cb0a6a66254eb135f7d8d33d9edce4552a7
Hirusha1-cpu <hirushafernando121@gmail.com>
2024-09-27 15:30:29 +0530

    updated

---
+++ frontend/src/Components/UserList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://172.16.26.236:8000/users')
+            .then(response => {
+                console.log(response);
+                
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the users!', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default UserList;
